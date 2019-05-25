6a80b070d1c820a6c5d79c1b22af782849bd5e1d
朱万友 <van@highsoft.ltd>
2019-05-25 21:12:29 +0800

    FizzBuzz v11

--- fizzbuzz/src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,37 @@
 package com.example.kata.fizzbuzz;
 
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class FizzBuzz {
-    ReportList run() {
-        return new ReportList(IntStream.range(1, 101).mapToObj(Report::new).toArray(Report[]::new));
+    String print() {
+        return IntStream.range(1, 101).mapToObj(this::tag).collect(Collectors.joining("\n"));
+    }
+
+    private String tag(int turn) {
+        if (isFizz(turn) && isBuzz(turn)) return "FizzBuzz";
+        if (isFizz(turn)) return "Fizz";
+        if (isBuzz(turn)) return "Buzz";
+        return String.valueOf(turn);
+    }
+
+    private boolean isFizz(int turn) {
+        return isDividableByOrContains(turn, 3);
+    }
+
+    private boolean isBuzz(int turn) {
+        return isDividableByOrContains(turn, 5);
+    }
+
+    private boolean isDividableByOrContains(int turn, int n) {
+        return contains(turn, n) || isDividableBy(turn, n);
+    }
+
+    private boolean contains(int turn, int n) {
+        return turn / 10 == n || turn % 10 == n;
+    }
+
+    private boolean isDividableBy(int turn, int n) {
+        return turn % n == 0;
     }
 }
--- fizzbuzz/src/test/java/com/example/kata/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/com/example/kata/fizzbuzz/FizzBuzzTest.java
@@ -6,8 +6,53 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void can_generate_100_turns() {
-        ReportList reportList = new FizzBuzz().run();
-        assertEquals(100, reportList.getNumberOfReports());
+    public void should_print_100_lines() {
+        assertEquals(100, new FizzBuzz().print().split("\n").length);
+    }
+
+    @Test
+    public void lines_should_be_turn_number_if_in_simple_turn() {
+        assertEquals("1", getPrintedListLine(0));
+        assertEquals("2", getPrintedListLine(1));
+    }
+
+    @Test
+    public void lines_should_be_fizz_if_turn_number_is_dividable_by_3() {
+        assertEquals("Fizz", getPrintedListLine(2));
+        assertEquals("Fizz", getPrintedListLine(5));
+    }
+
+    @Test
+    public void lines_should_be_buzz_if_turn_number_is_dividable_by_5() {
+        assertEquals("Buzz", getPrintedListLine(4));
+        assertEquals("Buzz", getPrintedListLine(9));
+    }
+
+    @Test
+    public void lines_should_be_fizzbuzz_if_turn_number_is_dividable_by_3_and_5() {
+        assertEquals("FizzBuzz", getPrintedListLine(14));
+        assertEquals("FizzBuzz", getPrintedListLine(44));
+    }
+
+    @Test
+    public void lines_should_be_fizz_if_turn_number_contains_3() {
+        assertEquals("Fizz", getPrintedListLine(12));
+        assertEquals("Fizz", getPrintedListLine(30));
+    }
+
+    @Test
+    public void lines_should_be_buzz_if_turn_number_contains_5() {
+        assertEquals("Buzz", getPrintedListLine(51));
+        assertEquals("Buzz", getPrintedListLine(55));
+    }
+
+    @Test
+    public void lines_should_be_fizzbuzz_if_turn_number_contains_3_and_5() {
+        assertEquals("FizzBuzz", getPrintedListLine(34));
+        assertEquals("FizzBuzz", getPrintedListLine(52));
+    }
+
+    private String getPrintedListLine(int n) {
+        return new FizzBuzz().print().split("\n")[n];
     }
 }
